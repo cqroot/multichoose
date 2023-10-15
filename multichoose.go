96dca6b582e75196c735a0403cb06ed471f8e18c
Chris W <cawatson1993@gmail.com>
2023-10-14 19:10:01 -0600

    allow for more than 64 items

--- multichoose.go
+++ multichoose.go
@@ -1,14 +1,36 @@
 package multichoose
 
 type MultiChoose struct {
-	selected uint64
+	selected []uint64
 	length   int
 	limit    int
 }
 
+func (mc *MultiChoose) getSelected(index int) bool {
+	if index < 0 || index > mc.length {
+		return false
+	}
+	i := uint64(index) >> 6
+	j := uint64(index) & 0x3F
+	return (mc.selected[i] & (1 << j)) != 0
+}
+
+func (mc *MultiChoose) setSelected(index int, value bool) {
+	if index < 0 || index > mc.length {
+		return
+	}
+	i := uint64(index) >> 6
+	j := uint64(index) & 0x3F
+	if value {
+		mc.selected[i] |= (1 << j)
+	} else {
+		mc.selected[i] &^= (1 << j)
+	}
+}
+
 func New(length int) *MultiChoose {
 	return &MultiChoose{
-		selected: 0,
+		selected: make([]uint64, (length>>6)+1),
 		length:   length,
 		limit:    -1,
 	}
@@ -17,8 +39,8 @@ func New(length int) *MultiChoose {
 func (mc MultiChoose) Count() int {
 	count := 0
 	for i := 0; i <= mc.length; i++ {
-		if mc.selected>>i&1 == 1 {
-			count += 1
+		if mc.getSelected(i) {
+			count++
 		}
 	}
 	return count
@@ -29,21 +51,13 @@ func (mc *MultiChoose) Select(index int) {
 		return
 	}
 
-	curr := uint64(1) << index
-	mc.selected = mc.selected | curr
+	mc.setSelected(index, true)
 }
 
 func (mc *MultiChoose) Deselect(index int) {
-	curr := uint64(1) << index
-	mc.selected = mc.selected & (^curr)
+	mc.setSelected(index, false)
 }
 
 func (mc MultiChoose) IsSelected(index int) bool {
-	i := uint64(index)
-	curr := uint64(1) << i
-	if (mc.selected & curr) != 0 {
-		return true
-	} else {
-		return false
-	}
+	return mc.getSelected(index)
 }
